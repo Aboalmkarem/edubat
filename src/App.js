4086f832b32121f7aabf3939f998d349e0433ca1
Mohamed Aboalmkarem <mo3ddeedd67983@gmail.com>
2024-02-07 16:52:46 +0200

    theme done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import useLocalStorage from 'use-local-storage';
+import Navbar from './componants/navbar/navbar'
 
 function App() {
+
+  const preference = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [isDark, setIsDark] = useLocalStorage("isDark", preference);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" data-theme={isDark ? "dark" : "light"}>
+      <Navbar isChecked={isDark} handleChange={() => {setIsDark(!isDark)}}></Navbar>
     </div>
   );
 }
